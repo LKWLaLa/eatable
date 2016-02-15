e52ac76dfdc96d3d3f50c9ae6cfa021cdf479e56
LKWLaLa <lkwlala@gmail.com>
2016-02-15 12:50:48 -0500

    add cli greeting with icons

--- lib/eatable/cli.rb
+++ lib/eatable/cli.rb
@@ -1,7 +1,24 @@
 class Eatable::CLI
 
-  def call
-    puts "Hello, World!"
+  AREAS = ["New York", "San Francisco", "Los Angeles", "Philadelphia", "Boston", 
+    "Chicago", "Washington, DC", "South Florida"]
+
+
+  def initialize
   end
+
+  def call
+    self.greeting
+  end
+
+  def greeting
+    puts "_" * 70, "\n"
+    puts "Hi there!  Welcome to Eatable!  \u{1f370} \u{1f374}"
+    puts "We are currently able to screen restaurants in the following areas:"
+    puts "_" * 70
+    AREAS.sort.each_with_index {|a, i| puts "#{i + 1}. #{a}" }    
+  end
+
+  
 
 end
